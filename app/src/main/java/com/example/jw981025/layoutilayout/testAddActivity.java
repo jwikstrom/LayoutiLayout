7006afb741d7ccba1900d4143a142d702925bd42
jwikstrom <jwikstrom56@gmail.com>
2016-09-22 11:13:47 +0200

    update. Added activity tesstAddActivity Changed int in Mainactivity

--- app/src/main/java/com/example/jw981025/layoutilayout/testAddActivity.java
+++ app/src/main/java/com/example/jw981025/layoutilayout/testAddActivity.java
@@ -3,18 +3,11 @@ package com.example.jw981025.layoutilayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class testAddActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test_add);
     }
-public void nymetord(){
-    int i = 1+1;
-
-    int ii = 2+1;
-
-    int iii = i + ii;
-}
 }
